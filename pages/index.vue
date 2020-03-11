5898692bf685a06e70e89ac95c44cad3d81fa41d
kaustubh619 <kaustubhkrishna9031@gmail.com>
2020-03-11 15:27:54 +0530

    now

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="logo-box">
+        <img src="~static/images/logo-white.png" alt="Logo" class="logo" />
+      </div>
+      <div class="text-box">
+        <h1 class="heading-primary">
+          <div class="heading-primary-main">Outdoors</div>
+          <div class="heading-primary-sub">is where life happens</div>
+        </h1>
+        <a href="#" class="btn btn-white btn-animate">Discover Our Tours</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  },
+
+  mounted() {},
+
+  methods: {}
+};
+</script>
+
+<style></style>
